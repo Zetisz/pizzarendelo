045aad225f63305c3cf4d0e50a3967423cf8141a
Zetisz <szabo.zeteny@petrik.hu>
2026-09-23 15:25:07 +0200

    Buttons, Label, & basic logic

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -19,5 +19,38 @@ public partial class MainWindow : Window
     public MainWindow()
     {
         InitializeComponent();
+        ListboxInit();
+    }
+
+    private void ListboxInit()
+    {
+        List<string> list = ["Margherita", "Sonkás", "Hawaii", "Négy sajtos", "Magyaros"];
+        foreach (var i in list)
+        {
+            Lb.Items.Add(i);
+        }
+    }
+    
+    private void GetIndex0()
+    {
+        var lbi = (ListBoxItem)
+            (Lb.ItemContainerGenerator.ContainerFromIndex(0));
+        Label.Content = lbi.Content.ToString();
+    }
+
+    private void BtnAdd_OnClick(object sender, RoutedEventArgs e)
+    {
+        if (!Lb.Items.Contains(TxtAdd.Text) && TxtAdd.Text != "")
+            Lb.Items.Add(TxtAdd.Text);
+    }
+
+    private void BtnDelete_OnClick(object sender, RoutedEventArgs e)
+    {
+        Lb.Items.Remove(Lb.SelectedItems[0]);
+    }
+
+    private void BtnOrder_OnClick(object sender, RoutedEventArgs e)
+    {
+        GetIndex0();
     }
 }
