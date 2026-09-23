3118bcd36b1c60650cef49c95b679f7b40bf9e61
Zetisz <szabo.zeteny@petrik.hu>
2026-09-23 16:29:26 +0200

    redesign & some error texts

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -17,7 +17,6 @@ namespace pizzarendelo;
 public partial class MainWindow : Window
 {
     private List<string> sizes = ["kicsi", "közepes", "nagy"];
-    private int num;
     public MainWindow()
     {
         InitializeComponent();
@@ -35,31 +34,57 @@ public partial class MainWindow : Window
     
     private void BtnAdd_OnClick(object sender, RoutedEventArgs e)
     {
+        RefreshError();
         if (!Lb.Items.Contains(TxtAdd.Text) && TxtAdd.Text != "")
             Lb.Items.Add(TxtAdd.Text);
     }
 
     private void BtnDelete_OnClick(object sender, RoutedEventArgs e)
     {
+        RefreshError();
+        if (Lb.SelectedItems.Count == 0) return;
         Lb.Items.Remove(Lb.SelectedItems[0]);
     }
 
     private void BtnOrder_OnClick(object sender, RoutedEventArgs e)
     {
-        
+        if (Lb.SelectedItems.Count == 0)
+        {
+            LabelError.Content = "Valasszon pizzat!";
+            return;
+        }
+        RefreshError();
         Label.Content = Lb.SelectedItems[0];
         if (sizes.Contains(TxtSize.Text))
         {
             var selectedPizza = Lb.SelectedItems[0] + " - " + TxtSize.Text;; 
             LbOrder.Items.Add(selectedPizza);
-
-            num++;
-            TxtNum.Text = num.ToString();
+            
+            LNum.Content = LbOrder.Items.Count;
+        }
+        else
+        {
+            LabelSizeError.Content = "Elfogadott méretek:  kicsi - közepes - nagy";
         }
     }
 
     private void BtnDeleteOrder_OnClick(object sender, RoutedEventArgs e)
     {
+        RefreshError();
+        if (LbOrder.SelectedItems.Count == 0) return;
         LbOrder.Items.Remove(LbOrder.SelectedItems[0]);
+    }
+    
+    private void BtnDeleteAll_OnClick(object sender, RoutedEventArgs e)
+    {
+        RefreshError();
+        LbOrder.Items.Clear();
+        LNum.Content = LbOrder.Items.Count;
+    }
+
+    private void RefreshError()
+    {
+        LabelError.Content = "";
+        LabelSizeError.Content = "";
     }
 }
